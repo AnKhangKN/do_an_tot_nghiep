756ff8cd9cf4ba4a9d458b4a6330f9730907ae9a
An Khang <minecraftcopyright1302@gmail.com>
2026-07-19 16:09:33 +0000

    update db

--- server/src/config/database.config.js
+++ server/src/config/database.config.js
@@ -20,25 +20,6 @@ const connectDB = async () => {
     const client = await pool.connect();
     console.log("Connected to PostgreSQL");
 
-    // ---------------------------------------
-
-    // Tự động tạo bảng rescuer_histories nếu chưa tồn tại
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS rescuer_histories (
-          rescuer_history_id UUID PRIMARY KEY,
-          rescuer_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-          sos_request_id UUID NOT NULL REFERENCES sos_requests(sos_request_id) ON DELETE CASCADE,
-          action VARCHAR(50) NOT NULL,
-          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
-      );
-      CREATE INDEX IF NOT EXISTS idx_rescuer_histories_rescuer_id ON rescuer_histories(rescuer_id);
-      CREATE INDEX IF NOT EXISTS idx_rescuer_histories_sos_request_id ON rescuer_histories(sos_request_id);
-    `;
-    await client.query(createTableQuery);
-    console.log("Database table 'rescuer_histories' verified/created successfully.");
-
-    // ---------------------------------------
-
     client.release();
   } catch (err) {
     console.error("DB Connection Error:", err.message);
